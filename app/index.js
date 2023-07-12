55dfa18eaa1643f6079f1ae2f4dbef7c1082ae4e
Francisco Costa <francisco100eg@gmail.com>
2023-07-12 17:17:31 -0300

    Feat: Navigation bar

--- app/index.js
+++ app/index.js
@@ -1,12 +1,47 @@
-import { View, Text} from 'react-native';
+import { useState } from "react";
 
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-const Home =()=> {
-    return (
-        <View>
-            <Text>Homeasasds</Text>
+import { COLORS, SIZES, SHADOWS, FONT, icons, images } from "../constants";
+
+import { ScreenHeaderBtn } from "../components/index";
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          title: "TotalVagas",
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: COLORS.primary,
+          },
+          headerTitleAlign: 'center',
+        
+        }}
+      ></Stack.Screen>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{
+            flex: 1,
+            padding: SIZES.medium,
+        }}>
+            <Text>Welcome</Text>
         </View>
-    )
-}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
