2b2554a90d4d9584463c5a1e90fd4e8b3cd031e4
Francisco Costa <francisco100eg@gmail.com>
2023-07-12 12:37:51 -0300

    Chore: Installed extra dependencies expo-font for fonts , axios for api requests and react-native-dotenv for variables setup

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text} from 'react-native';
+
+
+const Home =()=> {
+    return (
+        <View>
+            <Text>Homeasasds</Text>
+        </View>
+    )
+}
+
+export default Home;
